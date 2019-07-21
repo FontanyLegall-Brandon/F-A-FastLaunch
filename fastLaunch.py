438f6c54ceef55fe6815e37a2756c16595a76782
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2019-07-21 18:06:08 +0200

    fastLaunch

---
+++ fastLaunch.py
@@ -0,0 +1,42 @@
+##############################################################
+## FAST LAUNCH          
+##############################################################
+##
+## Author : =F|A= Scarface    
+## Licence : MIT              
+##                            
+##############################################################
+## CONFIGURATION         
+##############################################################
+
+servers = {"jay1":"jay1.clan-fa.com:27960",
+           "jay2":"jay2.clan-fa.com:27960",
+           "b1":"jay3.clan-fa.com",
+           "b2":"b2.clan-fa.com:27960",
+           "silent1":"silent1.clan-fa.com:27960",
+           "hc":"hc.clan-fa.com:27960",
+           "nq1":"nq1.clan-fa.com:27960",
+           "nq2":"nq2.clan-fa.com:27960",
+           "nq3":"nq3.clan-fa.com:27960",
+           "fun1":"fun1.clan-fa.com:27960",
+           "fun2":"fun2.clan-fa.com:27960",
+           "etl1":"etl1.clan-fa.com:27950"}
+
+          
+# Please insert into the toLaunch table the servers that you want to launch
+toLaunch = ["jay1"]
+
+# To launch all, use this above :
+#toLaunch = [key for key in servers.keys()]
+
+##############################################################
+## Script            
+##############################################################
+
+import subprocess
+program='ET.exe'
+
+for server in toLaunch :
+    if server in toLaunch.keys():
+        arguments={"+connect "+toLaunch[server])
+        subprocess.call([program, arguments])
